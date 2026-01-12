9f820f3214f0a47b09466cb25d6f538f5a003dfb
syahril45 <syahrilaswad45@gmail.com>
2026-01-13 01:32:06 +0800

    Update WauType.java

--- src/WauType.java
+++ src/WauType.java
@@ -5,7 +5,15 @@ public enum WauType {
     WAU_HELANG("wau_helang.png"),
     WAU_MERAK("wau_merak.png"),
     WAU_KAPAL("wau_kapal.png"),
-    WAU_SERI_BULAN("wau_seri_bulan.png");
+    WAU_SERI_BULAN("wau_seri_bulan.png"),
+    WAU_BARAT("wau_barat.png"),
+    WAU_KANGKANG("wau_kangkang.png"),
+    WAU_KEBAYAK("wau_kebayak.png"),
+    WAU_KENYALANG("wau_kenyalang.png"),
+    WAU_KIKIK("wau_kikik.png"),
+    WAU_PUYUH("wau_puyuh.png"),
+    WAU_SERI_NEGERI("wau_seri_negeri.png"),
+    WAU_KUCING("wau_kucing.png");
 
     public final String image;
 
